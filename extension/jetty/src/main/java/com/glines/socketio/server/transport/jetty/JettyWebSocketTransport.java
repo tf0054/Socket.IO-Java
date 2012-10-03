41db8f7ca7f1abd2921e017215742333dcc6dd32
Takeshi NAKANO <tf0054@bb.mbn.or.jp>
2012-10-03 22:16:46 +0900

    modify sample/eventbus for better understanding.

--- extension/jetty/src/main/java/com/glines/socketio/server/transport/jetty/JettyWebSocketTransport.java
+++ extension/jetty/src/main/java/com/glines/socketio/server/transport/jetty/JettyWebSocketTransport.java
@@ -137,10 +137,13 @@ public final class JettyWebSocketTransport extends AbstractTransport {
                 } else {
                     handler = session.getTransportHandler();
                 }
-
-                wsFactory.upgrade(request, response, WebSocket.class.cast(handler), protocol);
-                handler.sendMessage(new SocketIOFrame(SocketIOFrame.FrameType.CONNECT, SocketIOFrame.TEXT_MESSAGE_TYPE, ""));
-                handler.onConnect();
+                if(handler != null){
+                	wsFactory.upgrade(request, response, WebSocket.class.cast(handler), protocol);
+	                handler.sendMessage(new SocketIOFrame(SocketIOFrame.FrameType.CONNECT, SocketIOFrame.TEXT_MESSAGE_TYPE, ""));
+	                handler.onConnect();
+                } else {
+                    response.sendError(HttpServletResponse.SC_SERVICE_UNAVAILABLE, this + " handler is null");
+                }
             }
         } else {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, this + " transport error: Invalid request");
